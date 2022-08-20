11ff3a44fca0223a19395cffe2712d467e87405b
Ruslan Muslimov <alfa512ks@mail.ru>
2022-08-21 02:28:40 +0300

    GetIpAddress endpoint added

---
+++ AuthenticationApi/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationApi.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class InfoController : ControllerBase
+{
+    [HttpGet]
+    [Route("/ip-address")]
+    public async Task<string> GetIpAddress()
+    {
+        var remoteIpAddress = this.HttpContext.Request.HttpContext.Connection.RemoteIpAddress;
+        if (remoteIpAddress != null)
+            return await Task.FromResult(remoteIpAddress.ToString());
+        return string.Empty;
+    }
+}
